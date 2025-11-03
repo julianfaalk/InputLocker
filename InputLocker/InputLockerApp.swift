5b6fa0883526726e3d97403a605e7f8b48367357
jan0r <falk.julian@icloud.com>
2025-11-03 16:44:22 +0100

    Initial Commit

---
+++ InputLocker/InputLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputLockerApp.swift
+//  InputLocker
+//
+//  Created by Julian Falk on 03.11.25.
+//
+
+import SwiftUI
+
+@main
+struct InputLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
